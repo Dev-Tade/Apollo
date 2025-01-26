ea429d48f8420574de831059f51c209270cebc6d
Dev-Tade <88458312+Dev-Tade@users.noreply.github.com>
2025-01-26 18:18:55 -0300

    Add comments to common header helper macros

--- src/common.h
+++ src/common.h
@@ -3,6 +3,34 @@
 
 #include <stdint.h>
 
+/* 
+  -- Helper macro to declare header functions --
+
+  On definition:
+  #include "common.h"
+
+  would define APOLLO_DEF as extern
+
+  On an implementation:
+  #define APOLLO_IMPL
+  #include "common.h"
+
+  would define APOLLO_DEF as static
+
+  definition and implementation in same header:
+  #ifndef MY_HEADER_GUARD
+  #define MY_HEADER_GUARD
+
+  APOLLO_DEF void my_function(...);
+  
+  #endif // !MY_HEADER_GUARD
+  
+  #ifdef MY_HEADER_IMPLEMENTATION
+  
+  APOLLO_DEF my_function(...) {...}
+  
+  #endif // !MY_HEADER_IMPLEMENTATION
+*/
 #ifdef APOLLO_IMPL
   #undef APOLLO_DEF
   #define APOLLO_DEF static
@@ -11,6 +39,22 @@
   #define APOLLO_DEF extern
 #endif
 
+/*
+  -- Helper macro to determine underlying OS --
+  When system is Windows defines APOLLO_SYS_WINDOWS
+  if APOLLO_SYS_UNIX hasn't been defined and vice versa.
+  Will trigger an error if OS isn't one of them
+
+  Use case:
+  void *my_mmap(...)
+  {
+    #if defined(APOLLO_SYS_WINDOWS)
+      -- use CreateFileMapping, MapViewOfFile impl
+    #elif defined(APOLLO_SYS_UNIX)
+      -- use mmap impl
+    #endif
+  }
+*/
 #if (defined(_WIN32) || defined(_WIN64))
   #ifndef APOLLO_SYS_UNIX
   #define APOLLO_SYS_WINDOWS
