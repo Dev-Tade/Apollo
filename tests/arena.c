8bede00c6c8e9d4b91aa04cb55144405f00bac61
Dev-Tade <88458312+Dev-Tade@users.noreply.github.com>
2025-03-22 08:28:04 -0300

    snake_case on arena test

--- tests/arena.c
+++ tests/arena.c
@@ -4,48 +4,48 @@
 #include <string.h>
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-  Arena myArena = ARENA();
+  Arena my_arena = ARENA();
   
-  if (!arena_init(&myArena, 128)) {
+  if (!arena_init(&my_arena, 128)) {
     fprintf(stderr, "Failed to initialize memory arena\n");
     return 1;
   }
 
-  uint64_t *myU64 = (uint64_t*)arena_reserve(&myArena, sizeof(uint64_t));
-  if (myU64 == NULL) {
+  uint64_t *my_u64 = (uint64_t*)arena_reserve(&my_arena, sizeof(uint64_t));
+  if (my_u64 == NULL) {
     fprintf(stderr, "Arena reserve failed\n");
     return 1;
   }
 
-  *myU64 = 0xc0ffee12900d4e4e;
-  printf("myU64: %p -> %llu\n", myU64, *myU64);
+  *my_u64 = 0xc0ffee12900d4e4e;
+  printf("my_u64: %p -> %llu\n", my_u64, *my_u64);
 
-  char *myString = (char *)arena_reserve(&myArena, 64);
-  if (myString == NULL) {
+  char *my_string = (char *)arena_reserve(&my_arena, 64);
+  if (my_string == NULL) {
     fprintf(stderr, "Arena reserve failed\n");
     return 1;
   }
 
-  char *myStringLiteral = "coffee_is_good_hehe";
-  APOLLO_MEMCPY(myString, myStringLiteral, 20);
-  printf("myString: %p -> %s\n", myString, myString);
+  char *my_string_literal = "coffee_is_good_hehe";
+  APOLLO_MEMCPY(my_string, my_string_literal, 20);
+  printf("my_string: %p -> %s\n", my_string, my_string);
 
-  size_t remainingBytes = arena_left(&myArena);
-  printf("%d bytes are left on the arena\n", remainingBytes);
+  size_t remaining_bytes = arena_left(&my_arena);
+  printf("%d bytes are left on the arena\n", remaining_bytes);
 
-  size_t usedBytes = arena_used(&myArena);
-  printf("%d bytes are used on the arena\n", usedBytes);
+  size_t used_bytes = arena_used(&my_arena);
+  printf("%d bytes are used on the arena\n", used_bytes);
 
   #if 0
-  if (arena_reserve(&myArena, remainingBytes + 20) == NULL) {
+  if (arena_reserve(&my_arena, remaining_bytes + 20) == NULL) {
     fprintf(stderr, "Arena reserve failed (forced not enough memory)\n");
     return 1;
   }
   #endif
   #if 0
-  if (arena_reserve(NULL, remainingBytes + 20) == NULL) {
+  if (arena_reserve(NULL, remaining_bytes + 20) == NULL) {
     fprintf(stderr, "Arena reserve failed (forced not valid Arena)\n");
     return 1;
   }
