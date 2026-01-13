758939e4c2a04aedf2c65d55d3a9caa96293c8ea
Dev-Tade <88458312+Dev-Tade@users.noreply.github.com>
2026-01-13 18:13:34 -0300

    Upgrade STF: remove useless macros and factor out code so running all test doesn't require to call system()

--- src/stf.h
+++ src/stf.h
@@ -6,25 +6,25 @@
 
 /*
   -- Simple Test Framework --
-   # Terms:
-   > "module" = dynamic library / shared object (.dll, .so, .dylib)
-   > "list" and "all" are test cases implemented by stf CLI
-   # Command Line:
-   > stf <target-file.module> list   -- List all tests inside target module
-   > stf <target-file.module> all    -- Run all tests inside target module
-   > stf <target-file.module> <test> -- Run <test> from target module
-   # Writing Test:
-   > Any test module should include a function called stf_module_list with
-   the following signature `void stf_module_list(STF_MODULE_LIST)`
-   > The purpose of `stf_module_list` is register all the test in the module via:
-   stf_test_add(Test_List test_list, Test *test), its arguments are
-   pretty simple, `test_list` you can get this by using: `MODULE_LIST` macro
-  `test` argument is a pointer to a Test struct
-   > Building a Test structure: you can do it by using `TEST` macro with following
-   arguments: `TEST(function_name, description, argument)`, `function_name` is the
-   identifier of C function to run (max length 64 characters), `description` is 64
-   characters.
-   > What does a `test` return: true if succeeded or false if it didn't.
+  # Terms:
+  > "module" = dynamic library / shared object (.dll, .so, .dylib)
+  > "list" and "all" are test cases implemented by stf CLI
+  # Command Line:
+  > stf <target-file.module> list   -- List all tests inside target module
+  > stf <target-file.module> all    -- Run all tests inside target module
+  > stf <target-file.module> <test> -- Run <test> from target module
+  # Writing Test:
+  > Any test module should include a function called stf_module_list with
+  the following signature `void stf_module_list(stf_Tests *)`
+  > The purpose of `stf_module_list` is register all the test in the module via:
+  stf_register_test(stf_Tests *list, stf_Test *test), its arguments are
+  pretty simple, `list` or any other name, it' the argument passed to
+  `stf_module_list()`, `test` argument is a pointer to a stf_Test struct
+  > Building a Test structure: you can do it by using `TEST` macro with following
+  arguments: `STF_TEST_CASE(function_name, description, argument)`, `function_name` is the
+  identifier of C function to run (max length 64 characters), `description` is 64
+  characters.
+  > What does a `test` return?: true if succeeded or false if it didn't.
 */
 
 /*
@@ -45,7 +45,7 @@
   -- Module Type --
   Type alias to OS dynamic library / shared object handle
 */
-typedef void *Module;
+typedef void *stf_Module;
 
 /*
   -- Target Type --
@@ -53,7 +53,7 @@ typedef void *Module;
   Every test written should follow that pattern:
   `bool <test_name>(void)`
 */
-typedef uint32_t (*Target)(void);
+typedef bool (*stf_Target)(void);
 
 /*
   -- Test Entry Type/Struct --
@@ -63,7 +63,7 @@ typedef struct _stf_test
 {
   char target_string[64];
   char target_description[64];
-} Test;
+} stf_Test;
 
 /*
   -- Helper macro to converting function name to string --
@@ -74,9 +74,8 @@ typedef struct _stf_test
 /*
   -- Helper macro to build a Test Case --
 */
-#define TEST(function, description) (\
-  &(Test){__FN_STR__(function), description}\
-)
+#define STF_TEST_CASE(function, description) \
+  (&(stf_Test){__FN_STR__(function), description})
 
 /*
   -- Test_List Type/Struct --
@@ -84,34 +83,17 @@ typedef struct _stf_test
 */
 typedef struct _stf_test_list
 {
-  Test *tests;
+  stf_Test *items;
   size_t count;
   size_t capacity;
-} Test_List;
+} stf_Tests;
 
 /*
-  -- Helper macro to initialize an empty Test_List --
-*/
-#define TEST_LIST() ((Test_List){0})
-
-/*
-  -- Helper macro for stf_module_list argument --
-*/
-
-#define STF_MODULE_LIST Test_List *__module_list__
-
-/*
-  -- Helper macro for stf_test_add "list" parameter
-*/
-
-#define MODULE_LIST __module_list__
-
-/*
-  -- Register a test entry into a Test_List --
-  Should be called on stf_test_list for adding
+  -- Register a test entry into a stf_Tests list --
+  Should be called on stf_module_list for adding
   test entries, see "Writing Tests"
 */
-APOLLO_DEF void stf_test_add(Test_List *list, Test *test);
+APOLLO_DEF void stf_register_test(stf_Tests *tests, stf_Test *test);
 
 #endif //!STF_H
 
@@ -124,27 +106,27 @@ APOLLO_DEF void stf_test_add(Test_List *list, Test *test);
   -- ModuleEntryPoint Type --
   Type alias to a function pointer with a stf_module_list signature.
   Every test file should have that entry point:
-  `void stf_module_list(MODULE_LIST)`
+  `void stf_module_list(stf_Tests *tests)`
 */
-typedef void (*ModuleEntryPoint)(Test_List *);
+typedef void (*stf_ModuleEntryPoint)(stf_Tests *);
 
 /*
   -- Load a module --
   @param module_path: path to module file (C string)
-  @return `Module` handle, `NULL` if couldn't load properly
+  @return `stf_Module` handle, `NULL` if couldn't load properly
   @note Used internally by stf_cli
 */
-APOLLO_DEF Module stf_module_load(const char *module_path);
+APOLLO_DEF stf_Module stf_module_load(const char *module_path);
 
 /*
   -- Load function from module --
-  @param module: `Module` to load from
+  @param module: `stf_Module` to load from
   @param target: Name of the function to load (C string)
   @param quiet: 'true' if no error messages should be logged
-  @return `Target` function pointer, `NULL` if couldn't find `target`
+  @return `stf_Target` function pointer, `NULL` if couldn't find `target`
   @note Used internally by stf_cli
 */
-APOLLO_DEF Target stf_module_target(Module module, const char *target, bool quiet);
+APOLLO_DEF stf_Target stf_module_target(stf_Module module, const char *target, bool quiet);
 
 /*
   -- Unload a module --
@@ -152,7 +134,7 @@ APOLLO_DEF Target stf_module_target(Module module, const char *target, bool quie
   @return `May print some errors`
   @note Used internally by stf_cli
 */
-APOLLO_DEF void stf_module_unload(Module module);
+APOLLO_DEF void stf_module_unload(stf_Module module);
 
 /*
   -- Print manual for CLI --
@@ -190,6 +172,21 @@ APOLLO_DEF bool stf_cli_test(
   const char *stf_exec,
   const char *stf_target,
   const char *stf_test
+);
+
+/*
+  -- Run <test> of <module> --
+  @param stf_target: Path to module to load (C string)
+  @param module: Module from where the test was loaded
+  @param test: The test that is going to be ran
+  @return `false` on error (prints errors) or the result 
+  of the test ran 
+  @note Used internally by stf_cli
+*/
+APOLLO_DEF bool stf_run_test(
+  const char *stf_target,
+  const stf_Module module,
+  stf_Test *test
 );
 
 /*
@@ -234,9 +231,9 @@ APOLLO_DEF void stf_cli_manual(void)
   stf_log(" # Writing Tests: Checkout stf.h\n", 0);
 }
 
-APOLLO_DEF Module stf_module_load(const char *module_path)
+APOLLO_DEF stf_Module stf_module_load(const char *module_path)
 {
-  Module module = NULL;
+  stf_Module module = NULL;
 #if defined(APOLLO_SYS_WINDOWS)
   module = (void *)LoadLibraryA(module_path);
 #elif defined(APOLLO_SYS_UNIX)
@@ -251,7 +248,7 @@ APOLLO_DEF Module stf_module_load(const char *module_path)
   return module;
 }
 
-APOLLO_DEF void stf_module_unload(Module module)
+APOLLO_DEF void stf_module_unload(stf_Module module)
 {
 #if defined(APOLLO_SYS_WINDOWS)
   if(FreeLibrary(module) == 0) {
@@ -266,13 +263,13 @@ APOLLO_DEF void stf_module_unload(Module module)
 #endif
 }
 
-APOLLO_DEF Target stf_module_target(Module module, const char *target, bool quiet)
+APOLLO_DEF stf_Target stf_module_target(stf_Module module, const char *target, bool quiet)
 {
-  Target _target = NULL;
+  stf_Target _target = NULL;
 #if defined(APOLLO_SYS_WINDOWS)
-  _target = (Target)GetProcAddress(module, target);
+  _target = (stf_Target)GetProcAddress(module, target);
 #elif defined(APOLLO_SYS_UNIX)
-  _target = (Target)dlsym(module, target);
+  _target = (stf_Target)dlsym(module, target);
 #endif
 
   if (!_target && !quiet) {
@@ -282,10 +279,10 @@ APOLLO_DEF Target stf_module_target(Module module, const char *target, bool quie
   return _target;
 }
 
-APOLLO_DEF bool stf_module_load_test_list(const char *stf_target, Module module, Test_List *test_list)
+APOLLO_DEF bool stf_module_load_test_list(const char *stf_target, stf_Module module, stf_Tests *tests)
 {
   // Checking for NULL module should be done by caller
-  ModuleEntryPoint stf_module_list_entry = (ModuleEntryPoint)stf_module_target(module, "stf_module_list", true);
+  stf_ModuleEntryPoint stf_module_list_entry = (stf_ModuleEntryPoint)stf_module_target(module, "stf_module_list", true);
   
   if (!stf_module_list_entry) {
     stf_err(
@@ -297,10 +294,10 @@ APOLLO_DEF bool stf_module_load_test_list(const char *stf_target, Module module,
     return false;
   }
 
-  size_t prev_count = test_list->count;
-  stf_module_list_entry(test_list);
+  size_t prev_count = tests->count;
+  stf_module_list_entry(tests);
 
-  if (test_list->count <= prev_count) {
+  if (tests->count <= prev_count) {
     stf_err(
       "[ERROR]: module \"%s\" \"stf_module_list\" didn't add any test\n",
       stf_target
@@ -315,17 +312,16 @@ APOLLO_DEF bool stf_module_load_test_list(const char *stf_target, Module module,
 
 APOLLO_DEF bool stf_cli_list(const char *stf_exec, const char *stf_target)
 {
-  Module module = stf_module_load(stf_target);
+  stf_Module module = stf_module_load(stf_target);
   if (!module) return false;
 
-  Test_List test_list = TEST_LIST();
-  
-  if (!stf_module_load_test_list(stf_target, module, &test_list)) return false;
+  stf_Tests tests = {0};
+  if (!stf_module_load_test_list(stf_target, module, &tests)) return false;
 
-  for (size_t i=0; i < test_list.count; ++i) {
+  for (size_t i=0; i < tests.count; ++i) {
     stf_log("[TEST]: \"%s\" -- %s --\n",
-      test_list.tests[i].target_string,
-      test_list.tests[i].target_description
+      tests.items[i].target_string,
+      tests.items[i].target_description
     );
   }
 
@@ -335,23 +331,18 @@ APOLLO_DEF bool stf_cli_list(const char *stf_exec, const char *stf_target)
 
 APOLLO_DEF bool stf_cli_all(const char *stf_exec, const char *stf_target)
 {
-  Module module = stf_module_load(stf_target);
+  stf_Module module = stf_module_load(stf_target);
   if (!module) return false;
 
-  Test_List test_list = TEST_LIST();
+  stf_Tests tests = {0};
+  if (!stf_module_load_test_list(stf_target, module, &tests)) return false;
 
-  if (!stf_module_load_test_list(stf_target, module, &test_list)) return false;
-
-  size_t exec_count = test_list.count;
+  size_t exec_count = tests.count;
   size_t succeeded_count = 0;
 
   for (uint32_t i=0; i<exec_count; ++i) {
-    Test *test = &test_list.tests[i];
-
-    char buff[255] = {0};
-    snprintf(buff, 255, "%s %s %s\n", stf_exec, stf_target, test->target_string);
-    succeeded_count += (!system(buff));
-    stf_log("\n", 0);
+    stf_Test *test = &tests.items[i];
+    succeeded_count += stf_run_test(stf_target, module, test);
   }
 
   stf_log(
@@ -367,52 +358,58 @@ APOLLO_DEF bool stf_cli_all(const char *stf_exec, const char *stf_target)
 APOLLO_DEF bool stf_cli_test(
   const char *stf_exec,
   const char *stf_target,
-  const char *stf_test
-)
+  const char *stf_test)
 {
-  Module module = stf_module_load(stf_target);
+  stf_Module module = stf_module_load(stf_target);
   if (!module) return false;
 
-  Test_List test_list = TEST_LIST();
+  stf_Tests tests = {0};
+  if (!stf_module_load_test_list(stf_target, module, &tests)) return false;
 
-  if (!stf_module_load_test_list(stf_target, module, &test_list)) return false;
-
-  for (uint32_t i=0; i<test_list.count; ++i) {
-    Test *test = &test_list.tests[i];
+  for (uint32_t i=0; i<tests.count; ++i) {
+    stf_Test *test = &tests.items[i];
     
-    if (strcmp(stf_test, test->target_string) == 0) {
-      Target target = stf_module_target(module, test->target_string, true);
-      
-      if (target == NULL) {
-        stf_err(
-          "[INFO]: Ignoring case: \"%s\" (registered in \"%s\", unable to resolve its address)\n",
-          stf_test,
-          stf_target
-        );
-        return false;
-      }
-
-      stf_log(
-        "[INFO]: Executing case: \"%s\" (from \"%s\") -- %s --\n",
-        test->target_string, stf_target, test->target_description
-      );
-
-      bool succeeded = target();
-
-      stf_log(
-        "[INFO]: Finished case: \"%s\" (from \"%s\") -- %s (%s) --\n",
-        test->target_string, stf_target,  
-        succeeded ? "succeeded" : "didn't succeeded",
-        succeeded ? "true" : "false"
-      );
-
-      return succeeded;
-    }
+    if (strcmp(stf_test, test->target_string) == 0) 
+      return stf_run_test(stf_target, module, test);
   }
   
   stf_err("[ERROR]: No test named \"%s\" on module %s\n", stf_test, stf_target);
   stf_module_unload(module);
   return false;
+}
+
+APOLLO_DEF bool stf_run_test(
+  const char *stf_target,
+  const stf_Module module,
+  stf_Test *test)
+{
+  stf_Target target = stf_module_target(module, test->target_string, true);
+
+  if (target == NULL) {
+    stf_err(
+      "[INFO]: Ignoring case: \"%s\" (registered in \"%s\") unable to resolve it's address\n",
+      test->target_string,
+      stf_target
+    );
+
+    return false;
+  }
+
+  stf_log(
+    "[INFO]: Executing case: \"%s\" (from \"%s\") -- %s --\n",
+    test->target_string, stf_target, test->target_description
+  );
+
+  bool succeeded = target();
+
+  stf_log(
+    "[INFO]: Finished case: \"%s\" (from \"%s\") -- %s (%s) --\n",
+    test->target_string, stf_target,  
+    succeeded ? "succeeded" : "didn't succeeded",
+    succeeded ? "true" : "false"
+  );
+
+  return succeeded;
 }
 
 #endif //!STF_IMPL_CLI
@@ -422,37 +419,37 @@ APOLLO_DEF bool stf_cli_test(
 #define APOLLO_IMPL
 #include "apollo.h"
 
-APOLLO_DEF void stf_test_add(Test_List *list, Test *test)
+APOLLO_DEF void stf_register_test(stf_Tests *tests, stf_Test *test)
 {
   // Initialize dynamic array if not done yet
-  if (list->capacity == 0) {
+  if (tests->capacity == 0) {
     // Space for 16 tests by default seems reasonable
-    list->capacity = 16;
-    list->tests = (Test *)APOLLO_ALLOC(sizeof(Test) * list->capacity);
-    if (list->tests == NULL) {
+    tests->capacity = 16;
+    tests->items = (stf_Test *)APOLLO_ALLOC(sizeof(stf_Test) * tests->capacity);
+    if (tests->items == NULL) {
       stf_err("[ERROR]: Unable to allocate space for test list\n", 0);
       return;
     }
   }
 
   // Reallocate list if capacity exceeded
-  if ((list->count + 1) > list->capacity) {
+  if ((tests->count + 1) > tests->capacity) {
     // Allocate space for another 16 entries
-    list->capacity += 16;
-    list->tests = (Test *)APOLLO_REALLOC(
-      list->tests, 
-      sizeof(Test) * list->capacity
+    tests->capacity += 16;
+    tests->items = (stf_Test *)APOLLO_REALLOC(
+      tests->items, 
+      sizeof(stf_Test) * tests->capacity
     );
 
-    if (list->tests == NULL) {
+    if (tests->items == NULL) {
       stf_err("[ERROR]: Unable to reallocate space for test list\n", 0);
       return;
     }
   }
 
   // Actually add the test entry
-  Test *dest = &list->tests[list->count++];
-  if (APOLLO_MEMCPY(dest, test, sizeof(Test)) != dest) {
+  stf_Test *dest = &tests->items[tests->count++];
+  if (APOLLO_MEMCPY(dest, test, sizeof(stf_Test)) != dest) {
     stf_err("[ERROR]: Unable setup test entry for: %s\n", test->target_string);
     return;
   }
